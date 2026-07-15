7afea2ba16d69b2dd3ea1586eb62f47156284acf
Anvar Xusniddinov <anvarxsuniddinov7997@gmail.com>
2026-07-15 15:09:15 +0500

    update

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import UserViewSet
+from . import views
 
 urlpatterns = [
-    path('users/', UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list')
+    path('users/', views.UserViews.as_view(), name='user-list'),
+    path('users/create/', views.UserCreate.as_view(), name='user-create'),
+    path('users/update/<int:pk>/', views.UserUpdate.as_view(), name='user-update'),
+    path('users/delete/<int:pk>/', views.UserDelete.as_view(), name='user-delete'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,25 @@
-from . models import User
-from . serializers import UserSerializer
-from rest_framework import viewsets
+from django.shortcuts import render
+from .serializers import ProductsSerializer
+from rest_framework.generics import  ListAPIView,CreateAPIView,RetrieveDestroyAPIView,RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
-class UserViewSet(viewsets.ModelViewSet):
+class UserViews(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserCreate(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserUpdate(RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDelete(RetrieveDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
