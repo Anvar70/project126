3c2529a1236cd1042d828d48251191370d0506c6
Anvar Xusniddinov <anvarxsuniddinov7997@gmail.com>
2026-07-20 11:07:09 +0500

    crud

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.UserViews.as_view(), name='user-list'),
-    path('users/create/', views.UserCreate.as_view(), name='user-create'),
-    path('users/update/<int:pk>/', views.UserUpdate.as_view(), name='user-update'),
-    path('users/delete/<int:pk>/', views.UserDelete.as_view(), name='user-delete'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('users/', views.user_list, name='user-list'),
+    path('users/create/', views.user_create, name='user-create'),
+    path('users/update/<int:pk>/', views.user_update, name='user-update'),
+    path('users/delete/<int:pk>/', views.user_delete, name='user-delete'),
+    path('users/<int:pk>/', views.user_detail, name='user-detail'),
+    path('users/',views.user_view),             
+    path('users/<int:pk>/', views.user_view),
 ]
--- users/views.py
+++ users/views.py
@@ -1,25 +1,118 @@
-from django.shortcuts import render
-from .serializers import ProductsSerializer
-from rest_framework.generics import  ListAPIView,CreateAPIView,RetrieveDestroyAPIView,RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
 from .models import User
 from .serializers import UserSerializer
 
-class UserViews(ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
 
-class UserCreate(CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+@api_view(['GET'])
+def user_list(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
-class UserUpdate(RetrieveUpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+@api_view(['POST'])
+def user_create(request):
+    serializer = UserSerializer(data=request.data)
 
-class UserDelete(RetrieveDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class UserDetail(RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(
+            {"error": "User topilmadi"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def user_update(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(
+            {"error": "User topilmadi"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    serializer = UserSerializer(user, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def user_delete(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(
+            {"error": "User topilmadi"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    user.delete()
+    return Response(
+        {"message": "User o'chirildi"},
+        status=status.HTTP_204_NO_CONTENT
+    )
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def user_view(request, pk=None):
+
+    if request.method == 'GET' and pk is None:
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'GET' and pk is not None:
+        try:
+            user = User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return Response({"error": "User topilmadi"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'PUT':
+        try:
+            user = User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return Response({"error": "User topilmadi"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = UserSerializer(user, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        user = User.objects.get(pk=pk)
+        user.delete()
+        return Response(
+            {"message": "User o'chirildi"},
+            status=status.HTTP_204_NO_CONTENT
+        )
